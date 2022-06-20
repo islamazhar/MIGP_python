c0665b9b8e0d5c7bad9f849506a84c3eea4827a2
Mazharul Islam <mislam9@wisc.edu>
2022-06-20 14:16:13 -0700

    Updates

---
+++ performance_simulation/script_run_server.sh
@@ -0,0 +1,15 @@
+
+set -xe
+IDBServerPORT1=8774
+IDBServerPORT2=8775
+
+MIGPServerPORT1=8183 #n=100
+MIGPServerPORT2=8184 #n=100
+
+# Run the servers...
+python3.8 MIGP_server.py --port $MIGPServerPORT1 --prefix_len 16&
+python3.8 MIGP_server.py --port $MIGPServerPORT2 --prefix_len 20&
+
+python3.8 IDB_server.py --port $IDBServerPORT1 --prefix_len 16&
+python3.8 IDB_server.py --port $IDBServerPORT2 --prefix_len 20&
+wait
